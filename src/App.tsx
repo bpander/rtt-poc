12eda93cef29813d54df49fa900b47af1cf0ff78
Brad Anderson <>
2019-12-26 20:23:57 -0500

    Adds a rudimentary player stand-in

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Grid } from 'components/Grid';
 import { Vector2, Line2, Shape2 } from 'geo2d/core';
@@ -7,6 +7,7 @@ import { getNavMesh2d } from 'geo2d/navMesh2d';
 const scaleFactor = 50;
 
 const scaleVector2 = (v2: Vector2) => v2.map(n => n * scaleFactor) as Vector2;
+const descaleVector2 = (v2: Vector2) => v2.map(n => n / scaleFactor) as Vector2;
 const scaleLine2 = (line2: Line2) => line2.map(scaleVector2) as Line2;
 
 const colliders: Shape2[] = [
@@ -14,11 +15,19 @@ const colliders: Shape2[] = [
   [ [2, 4], [5, 4], [5, 5], [2, 5] ],
 ];
 
+const player = scaleVector2([10, 8]);
+
 export const App: React.FC = () => {
-  const links = getNavMesh2d(colliders);
+  const links = useMemo(() => getNavMesh2d(colliders), []);
+  const onAreaClick = (e: React.MouseEvent<SVGRectElement>) => {
+    const { left, top } = e.currentTarget.getBoundingClientRect();
+    const destination = descaleVector2([ e.clientX - left, e.clientY - top ]);
+    console.log(destination);
+  };
 
   return (
     <svg width={800} height={450} viewBox="0 0 800 450" style={{ border: '1px solid black' }}>
+      <rect width={800} height={450} fill="rgba(0, 0, 0, 0)" onClick={onAreaClick} />
       <Grid width={800} height={450} scale={scaleFactor} />
       <g stroke="black" strokeWidth="3" fill="rgba(0, 0, 0, 0.2)">
         {colliders.map((collider, i) => (
@@ -30,6 +39,7 @@ export const App: React.FC = () => {
           <line key={i} {...{x1, y1, x2, y2}} />
         ))}
       </g>
+      <circle cx={player[0]} cy={player[1]} r={15} fill="blue" />
     </svg>
   );
 };
